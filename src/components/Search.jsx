b81155f04d8950d83b817a0445d6799ebd7371ce
Uwa-T1m <chibuokemuwaechina@gmail.com>
2023-08-12 19:09:23 +0100

    initial commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import {RiSearch2Line} from 'react-icons/ri'
 
 const Search = () => {
   return (
-    <Container>
+    <Container className="form-control d-flex my-4 outline-0 border-0 shadow-none">
         <i><RiSearch2Line /></i>
         <input type="text" placeholder='Search for products'/>
     </Container>
